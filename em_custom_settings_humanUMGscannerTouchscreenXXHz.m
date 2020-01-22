a9dd1fbb7e58da20ed08bba02f79b65ea4e75a1e
TihanaHa <59601656+TihanaHa@users.noreply.github.com>
2020-01-22 21:01:16 +0100

    Update em_custom_settings_humanUMGscannerTouchscreenXXHz.m

--- em_custom_settings_humanUMGscannerTouchscreenXXHz.m
+++ em_custom_settings_humanUMGscannerTouchscreenXXHz.m
@@ -6,19 +6,19 @@
 settings = {...
 	
 	'SampleRate',		1000, ...		% Hz
-	'Downsample2Real',	60, ...			% Hz (actual eyetracker sampling rate), if 0 - do not downsample 
-	'SacOnsetVelThr',	30, ...			% deg/s
-	'SacOffsetVelThr',	10, ...			% deg/s
+	'Downsample2Real',	0, ...			% Hz (actual eyetracker sampling rate), if 0 - do not downsample 
+	'SacOnsetVelThr',	0.2, ...			% deg/s
+	'SacOffsetVelThr',	0.2, ...			% deg/s
 	'MinSacDuration',	0.03, ...		% s
 	'MaxSacDuration',	Inf, ...		% s
-	'MinSacAmplitude',	4, ...          % deg
+	'MinSacAmplitude',	0, ...          % deg
 	'MaxSacAmplitude',	Inf, ...		% deg
 	'PosSmoothConvWin',	'gausswin', ...	% 'rectwin', 'gausswin', etc. see 'help window'
 	'PosSmoothConvLen',	0, ...			% s, length of conv kernel, set to 0 of no smooting
-	'PosFilterCutoff',	30, ...			% Hz, set to 0 if no filter	
+	'PosFilterCutoff',	0, ...			% Hz, set to 0 if no filter	
 	'VelSmoothConvWin',	'gausswin', ...	% 'rectwin', 'gausswin', etc. see 'help window'
 	'VelSmoothConvLen',	0, ...			% s, length of conv kernel, set to 0 of no smooting
-	'VelFilterCutoff',	30, ...			% Hz, set to 0 if no filter
+	'VelFilterCutoff',	0, ...			% Hz, set to 0 if no filter
 	'VelAdaptiveThr',	false, ...		% true or false
     'MinFixDurAfterSac',0, ...          % s
 	'Plot',             true, ...       % true or false
