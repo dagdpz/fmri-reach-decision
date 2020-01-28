85d62fe48e6263cd71085fffec7fae01f8a738e0
TihanaHa <59601656+TihanaHa@users.noreply.github.com>
2020-01-28 16:50:27 +0100

    Update em_custom_settings_humanUMGscannerTouchscreenXXHz.m

--- em_custom_settings_humanUMGscannerTouchscreenXXHz.m
+++ em_custom_settings_humanUMGscannerTouchscreenXXHz.m
@@ -4,24 +4,23 @@
 % should contain variable **settings** as below!
 
 settings = {...
-	
-	'SampleRate',		1000, ...		% Hz
-	'Downsample2Real',	0, ...			% Hz (actual eyetracker sampling rate), if 0 - do not downsample 
-	'SacOnsetVelThr',	0.2, ...			% deg/s
-	'SacOffsetVelThr',	0.2, ...			% deg/s
-	'MinSacDuration',	0.03, ...		% s
-	'MaxSacDuration',	Inf, ...		% s
-	'MinSacAmplitude',	0, ...          % deg
-	'MaxSacAmplitude',	Inf, ...		% deg
-	'PosSmoothConvWin',	'gausswin', ...	% 'rectwin', 'gausswin', etc. see 'help window'
-	'PosSmoothConvLen',	0, ...			% s, length of conv kernel, set to 0 of no smooting
-	'PosFilterCutoff',	0, ...			% Hz, set to 0 if no filter	
-	'VelSmoothConvWin',	'gausswin', ...	% 'rectwin', 'gausswin', etc. see 'help window'
-	'VelSmoothConvLen',	0, ...			% s, length of conv kernel, set to 0 of no smooting
-	'VelFilterCutoff',	0, ...			% Hz, set to 0 if no filter
-	'VelAdaptiveThr',	false, ...		% true or false
+    'SampleRate',       1000, ...       % Hz
+    'Downsample2Real',  125, ...         % Hz (actual eyetracker sampling
+    'SacOnsetVelThr',   1, ...         % deg/s
+    'SacOffsetVelThr',  0.3, ...         % deg/s
+    'MinSacDuration',   0.03, ...       % s
+    'MaxSacDuration',   Inf, ...        % s
+    'MinSacAmplitude',  6, ...          % deg
+    'MaxSacAmplitude',  Inf, ...        % deg
+    'PosSmoothConvWin', 'gausswin', ... % 'rectwin', 'gausswin', etc.
+    'PosSmoothConvLen', 0 ...           % s, length of conv kernel, set
+    'PosFilterCutoff',  0, ...         % Hz, set to 0 if no filter 
+    'VelSmoothConvWin', 'gausswin', ... % 'rectwin', 'gausswin', etc.
+    'VelSmoothConvLen', 0.1, ...          % s, length of conv kernel, set 
+    'VelFilterCutoff',  0, ...         % Hz, set to 0 if no filter
+    'VelAdaptiveThr',   false, ...      % true or false
     'MinFixDurAfterSac',0, ...          % s
-	'Plot',             true, ...       % true or false
-	'OpenFigure',		true ...		% true or false
-	
+    'Plot',             true, ...       % true or false
+    'OpenFigure',       true ...        % true or false
+
 	};
