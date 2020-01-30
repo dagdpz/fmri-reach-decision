f333989a93b883cb55f0aadc69867347a9186ad2
TihanaHa <59601656+TihanaHa@users.noreply.github.com>
2020-01-30 15:34:26 +0100

    Update em_custom_settings_humanUMGscanner60Hz.m

--- em_custom_settings_humanUMGscanner60Hz.m
+++ em_custom_settings_humanUMGscanner60Hz.m
@@ -8,17 +8,17 @@ settings = {...
     'SampleRate',       1000, ...       % Hz
     'Downsample2Real',  60, ...         % Hz (actual eyetracker sampling 
     'SacOnsetVelThr',   30, ...         % deg/s
-    'SacOffsetVelThr',  10, ...         % deg/s
+    'SacOffsetVelThr',  9, ...         % deg/s
     'MinSacDuration',   0.03, ...       % s
     'MaxSacDuration',   Inf, ...        % s
-    'MinSacAmplitude',  4, ...          % deg
-    'MaxSacAmplitude',  Inf, ...        % deg
+    'MinSacAmplitude',  5, ...          % deg
+    'MaxSacAmplitude',  15, ...        % deg
     'PosSmoothConvWin', 'gausswin', ... % 'rectwin', 'gausswin', etc.
     'PosSmoothConvLen', 0 ...           % s, length of conv kernel, set
     'PosFilterCutoff',  30, ...         % Hz, set to 0 if no filter 
     'VelSmoothConvWin', 'gausswin', ... % 'rectwin', 'gausswin', etc.
     'VelSmoothConvLen', 0, ...          % s, length of conv kernel, set
-    'VelFilterCutoff',  30, ...         % Hz, set to 0 if no filter
+    'VelFilterCutoff',  0, ...         % Hz, set to 0 if no filter
     'VelAdaptiveThr',   false, ...      % true or false
     'MinFixDurAfterSac',0, ...          % s
     'Plot',             true, ...       % true or false
