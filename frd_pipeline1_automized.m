298265c730f54f8e4c5776bed75dfcdbfa27ce8e
peterneumann <45561899+peterneumann@users.noreply.github.com>
2020-02-14 12:13:34 +0100

    Update frd_pipeline1_automized.m

--- frd_pipeline1_automized.m
+++ frd_pipeline1_automized.m
@@ -1,31 +1,46 @@
 function frd_pipeline1_automized
+% those done already: CLSC,DAGU, ELRH, OLPE, (EVBO)
 
-load('Y:\MRI\Human\fMRI-reach-decision\Experiment\behavioral_data\protocols_exp.mat');
-prot(1) = [];
-prot(2) = [];
-prot(3) = [];
+clear all
 
+load('Y:\MRI\Human\fMRI-reach-decision\Experiment\behavioral_data\protocols_v2.mat');
+% throwaway = strcmp('OLPE',{prot.name}) | ...
+%     strcmp('ELRH',{prot.name}) |...
+%     strcmp('CLSC',{prot.name}) |...
+%     strcmp('DAGU',{prot.name}) |...
+%     strcmp('CLSC',{prot.name}) |...
+%     strcmp('EVBO',{prot.name}) |...
+%     strcmp('NORE',{prot.name}) |...
+%     strcmp('LORU',{prot.name}) |...
+%     strcmp('ANEL',{prot.name}) ;
 
+throwaway = ~strcmp('LORU',{prot.name});
+prot(throwaway) = [];
+prot.session(1) = [];
+
+ 
+ 
 runpath = 'D:\Experiment';
-%subjects = {'CLSC', 'DAGU', 'ELRH', 'EVBO', 'NORE', 'OLPE'};
+
+save('D:\Experiment\buffer_for_pipleine_1.mat','prot', 'runpath')
 
 for i = 1:length(prot)
     
     sessions = length(prot(i).session);
     
     for k = 1:sessions
-        
+                
         for m = 1 : length(prot(i).session(k).epi) 
             
             runpath_beh = [runpath filesep ... % Y:\MRI\Human\fMRI-reach-decision\Experiment\behavioral_data\CLSC\20200114\CLSC_2020-01-14_07.mat
                 'behavioral_data' filesep ...
-                prot(i).name{1} filesep ...
-                prot(i).session(k).date{1} filesep ...
+                prot(i).name filesep ...
+                prot(i).session(k).date filesep ...
                 prot(i).session(k).epi(m).mat_file ];
             
             runpath_mri = [runpath filesep ... % Y:\MRI\Human\fMRI-reach-decision\Experiment\CLSC\20200114
-                prot(i).name{1} filesep ...
-                prot(i).session(k).date{1}];
+                prot(i).name filesep ...
+                prot(i).session(k).date];
             
             if ~(exist([runpath_mri filesep prot(i).session(k).epi(m).mat_file],'file') == 2) % if it is not there already
                 copyfile(runpath_beh,[runpath_mri filesep])
@@ -43,14 +58,24 @@ for i = 1:length(prot)
 %         {'all'});
         
     ne_pl_process_one_session_3TUMG_part1(...
-        [runpath filesep prot(i).name{1} filesep prot(i).session(k).date{1}],... % session folder
-        [prot(i).session(k).hum{1} filesep 'dicom'],...                                         % hum_nummer
+        [runpath filesep prot(i).name filesep prot(i).session(k).date],... % session folder
+        [prot(i).session(k).hum filesep 'dicom'],...                                         % hum_nummer
         [prot(i).session(k).epi.nr1], ...                                  % first number of EPIs
         prot(i).session(k).T1.nr2,...                                      % secpnd number of T1
-        prot(i).name{1},...                                                         % subject name
+        prot(i).name,...                                                         % subject name
         'Human_reach_decision',...                                         % (see ne_pl_session_settings.m)
         {'all'});                                                          % sth
 
+    save('D:\Experiment\buffer_for_pipleine_1.mat','prot', 'runpath','i','k','m')
+    memory
+    inmem
+    
+    clear all
+    
+    load('D:\Experiment\buffer_for_pipleine_1.mat')
+    memory
+    inmem
+    
     end
 end 
 %%
