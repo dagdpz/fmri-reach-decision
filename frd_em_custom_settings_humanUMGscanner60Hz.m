9e8ca3e520dcc33b936b05ba4f2c4fdf9c933ad0
peterneumann <45561899+peterneumann@users.noreply.github.com>
2020-03-25 11:46:26 +0100

    Update frd_em_custom_settings_humanUMGscanner60Hz.m

--- frd_em_custom_settings_humanUMGscanner60Hz.m
+++ frd_em_custom_settings_humanUMGscanner60Hz.m
@@ -8,11 +8,11 @@ settings = {...
     'SampleRate',       1000, ...       % Hz
     'Downsample2Real',  60, ...         % Hz (actual eyetracker sampling 
     'SacOnsetVelThr',   30, ...         % deg/s
-    'SacOffsetVelThr',  9, ...          % deg/s
+    'SacOffsetVelThr',  10, ...          % deg/s
     'MinSacDuration',   0.03, ...       % s
-    'MaxSacDuration',   Inf, ...        % s
-    'MinSacAmplitude',  5, ...          % deg
-    'MaxSacAmplitude',  15, ...         % deg
+    'MaxSacDuration',   0.8, ...        % s
+    'MinSacAmplitude',  2.8, ...          % deg
+    'MaxSacAmplitude',  18, ...         % deg
     'PosSmoothConvWin', 'gausswin', ... % 'rectwin', 'gausswin', etc.
     'PosSmoothConvLen', 0 ...           % s, length of conv kernel, set
     'PosFilterCutoff',  30, ...         % Hz, set to 0 if no filter 
@@ -21,7 +21,7 @@ settings = {...
     'VelFilterCutoff',  30, ...          % Hz, set to 0 if no filter
     'VelAdaptiveThr',   false, ...      % true or false
     'MinFixDurAfterSac',0, ...          % s
-    'Plot',             false, ...       % true or false
+    'Plot',             true, ...       % true or false
     'OpenFigure',       false, ...        % true or false
     'Verbose',          false ...		% true or false
 	
