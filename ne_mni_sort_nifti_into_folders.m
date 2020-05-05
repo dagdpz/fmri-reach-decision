0a922cafe0e093ca0ee8fc86ef397761e3775260
Igor Kagan <igor.b.kagan@gmail.com>
2020-05-05 18:06:04 +0200

    Update ne_mni_sort_nifti_into_folders.m

--- ne_mni_sort_nifti_into_folders.m
+++ ne_mni_sort_nifti_into_folders.m
@@ -1,11 +1,11 @@
-function sort_nifti_into_folders(session_path,unzip_nifti_path,hum_nr,series_order,anat)
-% this function takes unziped EPIs and anatomical per session and puts them
-% in session_path in new folder structure
+function ne_mni_sort_nifti_into_folders(nifti_path_source,session_path,hum_nr,series_order,anat)
+% this function takes unziped EPIs and anatomical per session, after Carsten's topup correction script, from <nifti_path_source>, 
+% and puts them in <session_path> in new folder structure
 
-% sort_nifti_into_folders('D:\MRI\Human\fMRI-reach-decision\mni_Experiment\JOOD\20200207','D:\MRI\Human\fMRI-reach-decision\Carsten_folder','hum_14835',[6, 9, 12, 17, 20], 15)
+% ne_mni_sort_nifti_into_folders('D:\MRI\Human\fMRI-reach-decision\Carsten_folder','D:\MRI\Human\fMRI-reach-decision\mni_Experiment\JOOD\20200207','hum_14835',[6, 9, 12, 17, 20], 15)
 
 if series_order
-    for m = 1:length(series_order) % loop runs
+    for m = 1:length(series_order) % loop through EPI runs
         
         % extracting EPIs
         run_name = ['run' num2str(m,'%02d')];
@@ -14,7 +14,7 @@ if series_order
         if isempty(mess)
             disp([[session_path filesep run_name] ' created']);
             
-            gzipfilename = [unzip_nifti_path filesep hum_nr filesep 'nifti' filesep...
+            gzipfilename = [nifti_path_source filesep hum_nr filesep 'nifti' filesep...
                 hum_nr '_'...
                 sprintf('%04d',series_order(m)) '_'...
                 'topupcorr.nii.gz'];
@@ -35,7 +35,7 @@ if anat
     if isempty(mess)
         disp([[session_path filesep 'anat'] ' created']);
         
-        gzipfilename = [unzip_nifti_path filesep hum_nr filesep 'nifti' filesep...
+        gzipfilename = [nifti_path_source filesep hum_nr filesep 'nifti' filesep...
             hum_nr '_'...
             sprintf('%04d',anat)...
             '.nii.gz'];
@@ -45,7 +45,5 @@ if anat
     else
         disp([[session_path filesep 'anat'] ' already exists']);
     end
-end
-
 end
 
