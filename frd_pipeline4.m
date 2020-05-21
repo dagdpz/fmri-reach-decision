c0d85b1a855a1d1dfe3106e72ccc566f5f074735
peterneumann <45561899+peterneumann@users.noreply.github.com>
2020-05-21 18:17:18 +0200

    Create frd_pipeline4.m

---
+++ frd_pipeline4.m
@@ -0,0 +1,43 @@
+function frd_pipeline4
+
+clear all
+
+load('Y:\MRI\Human\fMRI-reach-decision\Experiment\behavioral_data\protocols_v2.mat');
+
+throwaway = ~strcmp('CAST',{prot.name})& ...
+      ~strcmp('EVBO',{prot.name}) &...    
+      ~strcmp('JAGE',{prot.name}) &...    
+      ~strcmp('LIKU',{prot.name}) &...    
+      ~strcmp('LEKU',{prot.name}) &...  
+      ~strcmp('LIKU',{prot.name}) &...
+      ~strcmp('LORU',{prot.name}) &...
+      ~strcmp('MABA',{prot.name}) &...
+      ~strcmp('MABL',{prot.name}) &...
+      ~strcmp('MARO',{prot.name}) &...
+      ~strcmp('PASC',{prot.name});
+
+%throwaway = ~strcmp('ELRH',{prot.name});
+
+prot(~throwaway) = [];
+
+
+runpath = 'D:\MRI\Human\fMRI-reach-decision\Experiment\TAL';
+
+
+    % glm cue centered
+    ne_pl_multisession_3TUMG_part2(...
+        [runpath],...                                   % basedir
+        {prot.name},...                                 % session_list
+        '',...                                          % dataset_path
+        ['Exp_combined_no_outliers_glm_cue'],...        % dataset_name
+        '*no_outliers_glm_cue.avg',...               % avg_name  '*no_outliers_glm_cue.avg',  '*combined_glm_cue.avg'
+        'Human_reach_decision',...                      % session_settings_id, (see ne_pl_session_settings.m)
+        {'all'},...                                     % proc_steps_array
+        'sdm_pattern',...                               % varargin
+            '*outlier_preds.sdm',...
+        'model',...
+            'mat2prt_reach_decision_vardelay_forglm');
+        
+        
+        
+        
