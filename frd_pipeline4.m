0fff343e57113e44190959fb0075add2082d5ea0
peterneumann <45561899+peterneumann@users.noreply.github.com>
2020-06-17 17:27:19 +0200

    Update frd_pipeline4.m

--- frd_pipeline4.m
+++ frd_pipeline4.m
@@ -10,21 +10,42 @@ throwaway = ~strcmp('CAST',{prot.name})& ...
       ~strcmp('LIKU',{prot.name}) &...    
       ~strcmp('LEKU',{prot.name}) &...  
       ~strcmp('LIKU',{prot.name}) &...
-      ~strcmp('LORU',{prot.name}) &...
       ~strcmp('MABA',{prot.name}) &...
       ~strcmp('MABL',{prot.name}) &...
       ~strcmp('MARO',{prot.name}) &...
       ~strcmp('PASC',{prot.name});
 
-%throwaway = ~strcmp('ELRH',{prot.name});
+
 
 prot(~throwaway) = [];
 
+session_list = {};
+for s = 1:length(prot)
+    for d = 1:length(prot(s).session)
+    sesh = {[prot(s).name filesep prot(s).session(d).date]};
+   session_list = [session_list sesh];
+   
+    end
+end
 
 runpath = 'D:\MRI\Human\fMRI-reach-decision\Experiment\TAL';
 
 
-    % glm cue centered
+    % glm cue centered MDM and GLM
+    ne_pl_multisession_3TUMG_part2(...
+        [runpath],...                                   % basedir
+        session_list,...                                 % session_list
+        '',...                                          % dataset_path
+        ['Exp_combined_no_outliers_glm_cue'],...        % dataset_name
+        '*no_outliers_glm_cue.avg',...               % avg_name  '*no_outliers_glm_cue.avg',  '*combined_glm_cue.avg'
+        'Human_reach_decision',...                      % session_settings_id, (see ne_pl_session_settings.m)
+        {'create_mdm'},...                                     % proc_steps_array
+        'sdm_pattern',...                               % varargin
+            '*outlier_preds.sdm',...
+        'model',...
+            'mat2prt_reach_decision_vardelay_forglm');
+        
+    % glm cue centered AVG
     ne_pl_multisession_3TUMG_part2(...
         [runpath],...                                   % basedir
         {prot.name},...                                 % session_list
@@ -32,12 +53,10 @@ runpath = 'D:\MRI\Human\fMRI-reach-decision\Experiment\TAL';
         ['Exp_combined_no_outliers_glm_cue'],...        % dataset_name
         '*no_outliers_glm_cue.avg',...               % avg_name  '*no_outliers_glm_cue.avg',  '*combined_glm_cue.avg'
         'Human_reach_decision',...                      % session_settings_id, (see ne_pl_session_settings.m)
-        {'all'},...                                     % proc_steps_array
+        {'create_avg'},...                                     % proc_steps_array
         'sdm_pattern',...                               % varargin
             '*outlier_preds.sdm',...
         'model',...
-            'mat2prt_reach_decision_vardelay_forglm');
-        
-        
+            'mat2prt_reach_decision_vardelay_forglm');        
         
         
