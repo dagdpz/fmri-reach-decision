5510ba1c2b8b48a0cb6a0b2d95b72694b6798635
peterneumann <45561899+peterneumann@users.noreply.github.com>
2020-06-17 17:23:41 +0200

    Update frd_pipeline3_automized.m

--- frd_pipeline3_automized.m
+++ frd_pipeline3_automized.m
@@ -10,13 +10,12 @@ throwaway = ~strcmp('CAST',{prot.name})& ...
       ~strcmp('LIKU',{prot.name}) &...    
       ~strcmp('LEKU',{prot.name}) &...  
       ~strcmp('LIKU',{prot.name}) &...
-      ~strcmp('LORU',{prot.name}) &...
       ~strcmp('MABA',{prot.name}) &...
       ~strcmp('MABL',{prot.name}) &...
       ~strcmp('MARO',{prot.name}) &...
       ~strcmp('PASC',{prot.name});
 
-%throwaway = ~strcmp('ELRH',{prot.name});
+% throwaway = strcmp('LORU',{prot.name});
 
 prot(~throwaway) = [];
 
@@ -25,27 +24,67 @@ runpath = 'D:\MRI\Human\fMRI-reach-decision\Experiment\TAL';
 
 save('D:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline.mat','prot', 'runpath')  % has to be hard coded, see below as well
 
+% %% glm cue centered
+% for i = 1:length(prot)
+%     
+%     
+%     ne_pl_multisession_3TUMG_part2(...
+%         [runpath filesep prot(i).name],...              % basedir
+%         {prot(i).session().date},...                    % session_list
+%         '',...                                          % dataset_path
+%         [prot(i).name '_combined_no_outliers_glm_cue'],...          % dataset_name (How to call the file) '_combined_no_outliers_glm_cue'
+%         '*no_outliers.avg',...                           % avg_name '*no_outliers.avg' '*forglm_cue.avg'
+%         'Human_reach_decision',...                      % session_settings_id, (see ne_pl_session_settings.m)
+%         {'all'},...                                     % proc_steps_array
+%         'sdm_pattern',...                               % varargin
+%             '*outlier_preds.sdm',...                    % '*MCparams.sdm'  '*outlier_preds.sdm'
+%         'model',...
+%             'mat2prt_reach_decision_vardelay_forglm');
+%     
+%         
+%         
+%         
+%     
+%     %% take care of memory
+%     save('D:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline.mat','prot', 'runpath','i')
+%     %memory
+%     %inmem
+%     
+%     clear all
+%     
+%     load('D:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline.mat')
+%     %memory
+%     %inmem
+%     
+%     
+% end
+
+%% avg's
+center = {'cue', 'mov'};
+delay = {'3', '6', '9', '12', '15'};
 
 for i = 1:length(prot)
     
-    %% glm cue centered
+    for c = 1:length(center)
+        
+        for d = 1:length(delay)
+
     ne_pl_multisession_3TUMG_part2(...
         [runpath filesep prot(i).name],...              % basedir
         {prot(i).session().date},...                    % session_list
         '',...                                          % dataset_path
-        [prot(i).name '_combined_no_outliers_glm_cue'],...          % dataset_name '_combined_no_outliers_glm_cue'
-        '*no_outliers.avg',...                           % avg_name '*no_outliers.avg' '*forglm_cue.avg'
+        [prot(i).name '_combined_avg_' center{c} '_' delay{d}'],...           % dataset_name '_combined_no_outliers_glm_cue'     (How the file will be called) 
+        ['*foravg_' center{c} '_' delay{d} '.avg'],...  % avg_name    '*no_outliers.avg' '*forglm_cue.avg' (Which avg from each session is being looked for) 
         'Human_reach_decision',...                      % session_settings_id, (see ne_pl_session_settings.m)
-        {'all'},...                                     % proc_steps_array
+        {'create_avg'},...                              % proc_steps_array
         'sdm_pattern',...                               % varargin
-            '*outlier_preds.sdm',...                    % '*MCparams.sdm'
+            '*MCparams.sdm',...                         % '*MCparams.sdm' '*outlier_preds.sdm'
         'model',...
-            'mat2prt_reach_decision_vardelay_forglm');
+            'mat2prt_reach_decision_vardelay_foravg');
     
-        
-    
+
     %% take care of memory
-    save('D:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline.mat','prot', 'runpath','i')
+    save('D:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline.mat','prot', 'runpath','i','c','d')
     %memory
     %inmem
     
@@ -54,6 +93,9 @@ for i = 1:length(prot)
     load('D:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline.mat')
     %memory
     %inmem
-    
+        end
+    end
     
 end
+
+
