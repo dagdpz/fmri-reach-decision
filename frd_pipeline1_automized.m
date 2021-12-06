2a0e6a6ce8791808ac65fff61e848d89a4d42078
peterneumann <45561899+peterneumann@users.noreply.github.com>
2021-12-06 14:07:40 +0100

    Update frd_pipeline1_automized.m

--- frd_pipeline1_automized.m
+++ frd_pipeline1_automized.m
@@ -1,38 +1,19 @@
-function frd_pipeline1_automized
-% those done already: CLSC,DAGU, ELRH, OLPE, (EVBO)
+function frd_pipeline1_automized(protocol_file,runpath,for_GLM,for_AVG)
+% wrapper function for preprocessing using NeuroElf.
+
+% You might want to change the location of the buffer file which has to be
+% hard coded. It exists, so the memory can be fully cleared in order for matlab
+% to not break after several iterations.  
 
 clear all
 
 %%
-load('Y:\MRI\Human\fMRI-reach-decision\Experiment\behavioral_data\protocols_v2.mat');
-% throwaway = strcmp('OLPE',{prot.name}) | ...
-%     strcmp('ELRH',{prot.name}) |...
-%     strcmp('CLSC',{prot.name}) |...
-%     strcmp('DAGU',{prot.name}) |...
-%     strcmp('CLSC',{prot.name}) |...
-%     strcmp('EVBO',{prot.name}) |...
-%     strcmp('NORE',{prot.name}) |...
-%     strcmp('LORU',{prot.name}) |...
-%     strcmp('ANEL',{prot.name}) ;
+% load('Y:\MRI\Human\fMRI-reach-decision\Experiment\behavioral_data\protocols_v2.mat');
+load(protocol_file);
 
-% uncomment for completed subjects
-% throwaway = ~strcmp('CAST',{prot.name})& ...
-%       ~strcmp('EVBO',{prot.name}) &...    
-%       ~strcmp('JAGE',{prot.name}) &...    
-%       ~strcmp('LIKU',{prot.name}) &...    
-%       ~strcmp('LEKU',{prot.name}) &...  
-%       ~strcmp('LIKU',{prot.name}) &...
-%       ~strcmp('LORU',{prot.name}) &...
-%       ~strcmp('MABA',{prot.name}) &...
-%       ~strcmp('MABL',{prot.name}) &...
-%       ~strcmp('MARO',{prot.name}) &...
-%       ~strcmp('PASC',{prot.name});
-
-throwaway = strcmp('OLPE',{prot.name});
-
-prot(~throwaway) = [];
-
-runpath = 'Y:\MRI\Human\fMRI-reach-decision\Experiment\MNI';
+% runpath = 'Y:\MRI\Human\fMRI-reach-decision\Experiment\MNI';
+% for_GLM = 0;
+% for_AVG = 0;
 
 %%
 save('Y:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline','prot', 'runpath') % has to be hard coded, so below as well
@@ -42,8 +23,8 @@ for i = 1:length(prot)
     sessions = length(prot(i).session);
     
     for k = 1:sessions
-                
-        for m = 1 : length(prot(i).session(k).epi) 
+        
+        for m = 1 : length(prot(i).session(k).epi)
             
             runpath_beh = [runpath filesep ... % Y:\MRI\Human\fMRI-reach-decision\Experiment\behavioral_data\CLSC\20200114\CLSC_2020-01-14_07.mat
                 'behavioral_data' filesep ...
@@ -60,61 +41,53 @@ for i = 1:length(prot)
             end
             
         end
+        %% example input
+        %     ne_pl_process_one_session_3TUMG_part1(...
+        %         'Y:\MRI\Human\fMRI-reach-decision\Pilot\MAPA\20190802',...
+        %         'hum_14406\dicom',...
+        %         [8 11 14 19 24], ...
+        %         6,...
+        %         'MAPA',...
+        %         'Human_reach_decision_pilot',...
+        %         {'all'},...
+        %         'beh2prt_function_handle',@mat2prt_reach_decision_vardelay_forglm);% respective function handle
         
-%     ne_pl_process_one_session_3TUMG_part1(...
-%         'Y:\MRI\Human\fMRI-reach-decision\Pilot\MAPA\20190802',...    
-%         'hum_14406\dicom',...
-%         [8 11 14 19 24], ...
-%         6,...
-%         'MAPA',...
-%         'Human_reach_decision_pilot',...
-%         {'all'});
-     
-%     % for GLM
-%     ne_pl_process_one_session_3TUMG_part1(...
-%         [runpath filesep prot(i).name filesep prot(i).session(k).date],... % session folder
-%         [prot(i).session(k).hum filesep 'dicom'],...                       % hum_nummer
-%         [prot(i).session(k).epi.nr1], ...                                  % first number of EPIs
-%         prot(i).session(k).T1.nr2,...                                      % secpnd number of T1
-%         prot(i).name,...                                                   % subject name
-%         'Human_reach_decision',...                                         % (see ne_pl_session_settings.m)
-%         {'all'},...
-%         'beh2prt_function_handle',@mat2prt_reach_decision_vardelay_forglm);% respective function handle                                                         
-    
-    % for AVG
-       ne_pl_process_one_session_3TUMG_part1(...
-        [runpath filesep prot(i).name filesep prot(i).session(k).date],... % session folder
-        [prot(i).session(k).hum filesep 'dicom'],...                       % hum_nummer
-        [prot(i).session(k).epi.nr1], ...                                  % first number of EPIs
-        prot(i).session(k).T1.nr2,...                                      % secpnd number of T1
-        prot(i).name,...                                                   % subject name
-        'Human_reach_decision',...                                         % (see ne_pl_session_settings.m)
-        {'create_prt'},...                                                 % sth 
-        'beh2prt_function_handle',@mat2prt_reach_decision_vardelay_foravg);%
-    
-
-    save('Y:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline','prot', 'runpath','i','k','m')
-    %memory
-    %inmem
-    
-    clear all
-    
-    load('Y:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline')
-    %memory
-    %inmem
-    
+        % for GLM
+        if for_GLM
+            ne_pl_process_one_session_3TUMG_part1(...
+                [runpath filesep prot(i).name filesep prot(i).session(k).date],... % session folder
+                [prot(i).session(k).hum filesep 'dicom'],...                       % hum_nummer
+                [prot(i).session(k).epi.nr1], ...                                  % first number of EPIs
+                prot(i).session(k).T1.nr2,...                                      % secpnd number of T1
+                prot(i).name,...                                                   % subject name
+                'Human_reach_decision',...                                         % (see ne_pl_session_settings.m)
+                {'all'},...
+                'beh2prt_function_handle',@mat2prt_reach_decision_vardelay_forglm);% respective function handle
+        end
+        
+        % for AVG
+        if for_AVG
+            ne_pl_process_one_session_3TUMG_part1(...
+                [runpath filesep prot(i).name filesep prot(i).session(k).date],... % session folder
+                [prot(i).session(k).hum filesep 'dicom'],...                       % hum_nummer
+                [prot(i).session(k).epi.nr1], ...                                  % first number of EPIs
+                prot(i).session(k).T1.nr2,...                                      % secpnd number of T1
+                prot(i).name,...                                                   % subject name
+                'Human_reach_decision',...                                         % (see ne_pl_session_settings.m)
+                {'create_prt'},...                                                 % sth
+                'beh2prt_function_handle',@mat2prt_reach_decision_vardelay_foravg);%
+        end
+        
+        save('Y:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline','prot', 'runpath','i','k','m')
+        %memory
+        %inmem
+        
+        clear all
+        
+        load('Y:\MRI\Human\fMRI-reach-decision\Experiment\buffer_for_pipeline')
+        %memory
+        %inmem
+        
     end
-end 
-%%
-% 
-%     ne_pl_process_one_session_3TUMG_part1(...
-%         [runpath filesep prot(i).name{1} filesep prot(i).session(k).date{1}],... % session folder
-%         [prot(i).session(k).hum{1} filesep 'dicom'],...                                         % hum_nummer
-%         [prot(i).session(k).epi.nr1], ...                                  % first number of EPIs
-%         prot(i).session(k).T1.nr2,...                                      % secpnd number of T1
-%         prot(i).name{1},...                                                         % subject name
-%         'Human_reach_decision',...                                         % (see ne_pl_session_settings.m)
-%         {'all'});       
-% 
-% ne_pl_process_one_session_3TUMG_part1('Y:\MRI\Human\fMRI-reach-decision\Experiment\CLSC\20200114','hum_14741\dicom', [ 6     9    12    17    20], 15,'CLSC','Human_reach_decision',{'all'});  
+end
 
